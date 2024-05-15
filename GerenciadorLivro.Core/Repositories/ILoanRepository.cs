4e713cb7e95c3861948367c61dda070f65078aa1
monegasco <mhoa.2000@gmail.com>
2024-05-14 22:33:20 -0300

    feat: add interface repository loan

---
+++ GerenciadorLivro.Core/Repositories/ILoanRepository.cs
@@ -0,0 +1,9 @@
+﻿using GerenciadorLivro.Core.Entities;
+
+namespace DevFreela.Core.Repositories;
+
+public interface ILoanRepository
+{
+    Task AddAsync(Loan loan);
+    Task ReturnBook(DateTime returnDate);
+}
