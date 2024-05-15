4d17609496a7a34934d2695807f24222cac72177
monegasco <mhoa.2000@gmail.com>
2024-05-14 22:36:00 -0300

    feat: add pagination result

--- GerenciadorLivro.Core/Repositories/IBookRepository.cs
+++ GerenciadorLivro.Core/Repositories/IBookRepository.cs
@@ -1,11 +1,12 @@
 ﻿using GerenciadorLivro.Core.Entities;
+using GerenciadorLivro.Core.Models;
 
 namespace DevFreela.Core.Repositories;
 
 public interface IBookRepository
 {
     Task AddAsync(Book book);
-    Task<List<Book>> GetAllAsync();
+    Task<PaginationResult<Book>> GetAllAsync(string query, int page = 1);
     Task<Book> GetByIdAsync(int id);
     Task DeleteByIdAsync(int id);
 }
