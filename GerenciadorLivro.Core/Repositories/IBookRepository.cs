fee2ba68b58b0beec9e164720b3822d93de00d4c
monegasco <mhoa.2000@gmail.com>
2024-05-14 22:29:26 -0300

    feat: add interface repository book

---
+++ GerenciadorLivro.Core/Repositories/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using GerenciadorLivro.Core.Entities;
+
+namespace DevFreela.Core.Repositories;
+
+public interface IBookRepository
+{
+    Task AddAsync(Book book);
+    Task<List<Book>> GetAllAsync();
+    Task<Book> GetByIdAsync(int id);
+    Task DeleteByIdAsync(int id);
+}
