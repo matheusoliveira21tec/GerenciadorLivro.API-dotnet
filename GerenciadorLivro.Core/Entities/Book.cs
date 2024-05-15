6c64e7cdd1b55066294bfe272cd815d377aaacd4
monegasco <mhoa.2000@gmail.com>
2024-05-14 22:05:05 -0300

    feat: add entity book

---
+++ GerenciadorLivro.Core/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Core.Entities;
+
+namespace GerenciadorLivro.Core.Entities;
+
+public class Book : BaseEntity
+{
+    public Book(string title, string author, string iSBN) : base()
+    {
+        Title = title;
+        Author = author;
+        ISBN = iSBN;
+    }
+
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public string ISBN { get; private set; }
+}
