43d2403b25788ce9d8b0885741f0c3105347a87c
monegasco <mhoa.2000@gmail.com>
2024-05-14 21:52:58 -0300

    feat: add entity baseEntity

---
+++ GerenciadorLivro.Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DevFreela.Core.Entities;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity()
+    {
+        CreatedAt = DateTime.Now;
+    }
+    public int Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+}
