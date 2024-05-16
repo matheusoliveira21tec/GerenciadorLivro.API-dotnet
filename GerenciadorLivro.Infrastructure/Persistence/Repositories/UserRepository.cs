f4c4a86531ebe6d22f7fcb05b2b0cf96e740470f
monegasco <mhoa.2000@gmail.com>
2024-05-15 22:33:13 -0300

    feat: add user repository

---
+++ GerenciadorLivro.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Core.Repositories;
+using GerenciadorLivro.Core.Entities;
+
+namespace GerenciadorLivro.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly GerenciadorLivroDbContext _dbContext;
+        public UserRepository(GerenciadorLivroDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
