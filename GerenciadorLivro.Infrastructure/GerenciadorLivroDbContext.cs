1d18ee335977d1be9a63d520a4c09bbda15bf983
monegasco <mhoa.2000@gmail.com>
2024-05-15 22:28:37 -0300

    feat: add dbcontext

---
+++ GerenciadorLivro.Infrastructure/GerenciadorLivroDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GerenciadorLivro.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GerenciadorLivro.Infrastructure;
+
+public class GerenciadorLivroDbContext : DbContext
+{
+    public GerenciadorLivroDbContext(DbContextOptions<GerenciadorLivroDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
+   
